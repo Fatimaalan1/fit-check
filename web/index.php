0694346d156472c4fbbefb24d98f996a4c3e85d8
Sayma Haque <sayma.haque@ucalgary.ca>
2024-11-01 13:44:54 -0600

    inital sql db, schema script

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FitCheck</title>
+</head>
+<body>
+    <div class="login-box">
+        <h2>~ Welcome ~</h2>
+       
+        <h2>Welcome to FitCheck</h2>
+    </div>
+</body>
+</html>
